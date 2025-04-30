af51ea219c99d542641925df239faaea07d645f1
NiharPy <155871278+NiharPy@users.noreply.github.com>
2025-04-30 13:31:41 +0530

    otp

--- backend/utils/otpService.js
+++ backend/utils/otpService.js
@@ -1,7 +1,7 @@
 import twilio from "twilio";
 
 const accountSid = 'AC1fc612fc64686e2288093d6c0df74ff2';
-const authToken = 'c8abb74a45df846024338806e76b2ebb';
+const authToken = '22628bdeb32b11f84ea4bd0002ef99cd';
 const messagingServiceSid = 'MGcb77ccadf5b4df8c8dc11d25b06f4e4d';
 
 const client = twilio(accountSid, authToken);
