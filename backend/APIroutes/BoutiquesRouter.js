d18a3f9f14401fcf2eccba82aacc06520655f0c5
NiharPy <155871278+NiharPy@users.noreply.github.com>
2025-06-11 18:39:28 +0530

    broute-update

--- backend/APIroutes/BoutiquesRouter.js
+++ backend/APIroutes/BoutiquesRouter.js
@@ -18,48 +18,48 @@ router.route("/search").get(boutiqueSearch);
 
 router.route('/:boutiqueId/track').get(authMiddleware, trackBusiness);
 
-router.route('/:boutiqueId/order').get(getBoutiqueOrders);
+router.route('/:boutiqueId/order').get(authMiddleware,getBoutiqueOrders);
 
 router.route('/:boutiqueId/order-completed').get(authMiddleware,getCompletedOrders);
 
-router.route('/:boutiqueId/order/:orderId/status').post(updateOrderStatus);
+router.route('/:boutiqueId/order/:orderId/status').post(authMiddleware,updateOrderStatus);
 
 router.route('/:boutiqueId/order/:orderId').get(authMiddleware,getOrderDetails);
 
-router.route("/:boutiqueId/order/:orderId/accept").post(acceptOrder);
+router.route("/:boutiqueId/order/:orderId/accept").post(authMiddleware,acceptOrder);
 
 router.route("/:boutiqueId/order/:orderId/decline").post(authMiddleware, declineOrder);
 
-router.route("/:boutiqueId/catalogue").get(getBoutiqueCatalogue);
+router.route("/:boutiqueId/catalogue").get(authMiddleware,getBoutiqueCatalogue);
 
-router.route("/:boutiqueId/dresstypes").get(getDressTypesWithDetails);
+router.route("/:boutiqueId/dresstypes").get(authMiddleware,getDressTypesWithDetails);
 
-router.route("/:boutiqueId/add-catalogue-item").post(addItemToCatalogue);
+router.route("/:boutiqueId/add-catalogue-item").post(authMiddleware,addItemToCatalogue);
 
-router.route("/:boutiqueId/delete-catalogue-item").delete(deleteItemFromCatalogue);
+router.route("/:boutiqueId/delete-catalogue-item").delete(authMiddleware,deleteItemFromCatalogue);
 
 router.post(
     '/:boutiqueId/add-dress-type',
     upload.fields([{ name: 'images', maxCount: 5 }]),
-    addDressType
+    authMiddleware,addDressType
   );
   
 
-router.route("/:boutiqueId/delete-dressType").delete(deleteDressType);
+router.route("/:boutiqueId/delete-dressType").delete(authMiddleware,deleteDressType);
 
-router.route('/:boutiqueId/review-alteration/:requestId').put(reviewAlterationRequest);
+router.route('/:boutiqueId/review-alteration/:requestId').put(authMiddleware,reviewAlterationRequest);
 
-router.route('/:boutiqueId/alteration/respond').put(respondToAlterationRequest);
+router.route('/:boutiqueId/alteration/respond').put(authMiddleware,respondToAlterationRequest);
 
-router.route('/:boutiqueId/alterations').get(getAlterationRequestsForBoutique);
+router.route('/:boutiqueId/alterations').get(authMiddleware,getAlterationRequestsForBoutique);
 
-router.route('/:boutiqueId/alterations/active').get(getActiveAlterationRequests);
+router.route('/:boutiqueId/alterations/active').get(authMiddleware,getActiveAlterationRequests);
 
-router.route("/:boutiqueId/createBill").post(createBill);
+router.route("/:boutiqueId/createBill").post(authMiddleware,createBill);
 
-router.route('/:boutiqueId/generate-invoice/:orderId').get(generateInvoice);
+router.route('/:boutiqueId/generate-invoice/:orderId').get(authMiddleware,generateInvoice);
 
-router.route("/:boutiqueId/PaidOrders").get(getOrdersByStatus);
+router.route("/:boutiqueId/PaidOrders").get(authMiddleware,getOrdersByStatus);
 
 
 
