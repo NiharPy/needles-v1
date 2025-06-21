11783ef660544d1ea3f57f3e24bd79debee54785
NiharPy <155871278+NiharPy@users.noreply.github.com>
2025-06-21 17:26:53 +0530

    new admin update

--- backend/models/AdminSchema.js
+++ backend/models/AdminSchema.js
@@ -3,8 +3,7 @@ import mongoose from 'mongoose';
 const adminSchema = new mongoose.Schema({
   username: { 
     type: String, 
-    required: true, 
-    unique: true 
+    required: true
   },
   phone: { 
     type: String, 
